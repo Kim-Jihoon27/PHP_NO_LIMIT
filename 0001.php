317f8d6ebe54387c72b5de5d2ed85df422b9727d
Kim-Jihoon27 <edica.discovery@gmail.com>
2026-04-08 21:11:12 +0800

    Initial Commit - 0001

---
+++ 0001.php
@@ -0,0 +1,20 @@
+<?php 
+
+#Problem -  Implement a script that prints Hello World.
+
+
+$name = "Kim Jihoon";
+
+echo $name . "\n";   // newline in CLI
+var_dump($name);
+
+# Output: ----------
+
+#Kim Jihoon
+#string(10) "Kim Jihoon"
+
+
+#Explain_By_Kim_Jihoon
+
+#by writting a basic variable that start with "$" dollar we can output anything inside like numbers or characters
+#I used var_dump() to output 
